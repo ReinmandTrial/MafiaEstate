14eab4946f751be15c509c2cab350a2cad7b6a19
Reinmand Trial <max.gluschtsov@gmail.com>
2021-08-25 00:54:08 +0300

    add slider 100% - need qa. responsive footer - need qa. responsive nft, - need qa

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,27 @@
-new Swiper('.team__slider', {
+new Swiper('.team-slider', {
 	spaceBetween: 59,
-	slidesPerView: 4,
+	slidesPerView: 3.09,
 	mousewheel: {
 		sensitivity: 1,
 	},
 	breakpoints: {
+		1280: {
+			slidesPerView: 3.09,
+		},
+		768: {
+			slidesPerView: 4,
+			spaceBetween: 40,
+		},
 		// 769: {
 		// },
-		// 576: {
-		// },
-		// 320: {
-		// }
+		576: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+		320: {
+			slidesPerView: 2.5,
+			spaceBetween: 20,
+		},
 	},
 });
 new Swiper('.slider-people', {
@@ -26,23 +37,40 @@ new Swiper('.slider-people', {
 	loop: true,
 	// effect: 'fade',
 });
-new Swiper('.store__slider', {
+
+new Swiper('.store-slider', {
 	pagination: {
 		el: '.swiper-pagination',
 		type: 'bullets',
 	},
-	spaceBetween: 95,
-	slidesPerView: auto,
+	// spaceBetween: 95,
+	// slidesPerView: 1.68,
 	mousewheel: {
 		sensitivity: 1,
 	},
 	breakpoints: {
-		// 769: {
-		// },
-		// 576: {
-		// },
-		// 320: {
-		// }
+		1439: {
+			spaceBetween: 95,
+			slidesPerView: 1.68,
+		},
+		1280: {
+			spaceBetween: 75,
+			slidesPerView: 1.8,
+		},
+		992: {
+			slidesPerView: 2.2,
+		},
+		769: {
+			spaceBetween: 30,
+			// slidesPerView: 2,	
+		},
+		576: {
+			spaceBetween: 25,
+			slidesPerView: 2,
+		},
+		320: {
+			spaceBetween: 25,
+			slidesPerView: 1.7,
+		},
 	},
-
 });
