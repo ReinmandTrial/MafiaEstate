8cf62b666f9d0a85e6f9cc33052af5f7f6afcffa
kirill.pr9n1k <kirill.pr9n1k@yandex.ru>
2021-08-24 21:56:57 +0300

    burger menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,39 @@
+$(document).ready(function(){
+	// burber menu 
+	$('.burger-btn').on('click',function(){
+		$('.burger-menu').addClass('open');
+		$('.burger-menu-bg').fadeIn();
+	})
+
+	// 
+    $(document).on('click', function (e) {
+		if (!$(e.target).closest(".burger-btn").length ) {
+			$('.burger-menu').removeClass('open');
+			$('.burger-menu-bg').fadeOut();
+		}
+		e.stopPropagation();
+	});
+	//burger menu end 
+
+	//плавная прокрутка якоря
+	const anchors = document.querySelectorAll('a[href*="#"]')
+
+	for (let anchor of anchors) {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault()
+
+			const blockID = anchor.getAttribute('href').substr(1)
+
+			document.getElementById(blockID).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start'
+			})
+		})
+	}
+
+})
+
+
 new Swiper('.team__slider', {
 	spaceBetween: 59,
 	slidesPerView: 4,
@@ -43,6 +79,5 @@ new Swiper('.store__slider', {
 		// },
 		// 320: {
 		// }
-	},
-
+	}
 });
